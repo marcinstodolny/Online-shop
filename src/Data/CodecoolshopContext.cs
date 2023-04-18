ef9fac80f1bd7a59bca05c8ea44bc84e4fe22fd4
Jacek Walaszek <jwalaszek@centonit.com>
2023-04-18 13:03:49 +0200

    using IConfiguration to get connection string for context

--- src/Data/CodecoolshopContext.cs
+++ src/Data/CodecoolshopContext.cs
@@ -1,25 +1,29 @@
 ﻿using Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Data
 {
     public class CodecoolshopContext : DbContext
     {
-        private const string ConnectionString = "Data Source=localhost;Database=Codecoolshop;Trust Server Certificate=true;MultipleActiveResultSets=true;Integrated Security=true";
+        //private const string ConnectionString = "Data Source=localhost;Database=Codecoolshop;Trust Server Certificate=true;MultipleActiveResultSets=true;Integrated Security=true";
+        private readonly IConfiguration _configuration;
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
         public DbSet<Supplier> Suppliers { get; set; }
-        public CodecoolshopContext(DbContextOptions options) : base(options)
+        public CodecoolshopContext(DbContextOptions options, IConfiguration configuration) : base(options)
         {
+            _configuration = configuration;
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
+                string connectionString = _configuration.GetConnectionString("CodecoolShopConnectionString");
                 optionsBuilder
-                    .UseSqlServer(ConnectionString)
+                    .UseSqlServer(connectionString)
                     .LogTo(Console.WriteLine, LogLevel.Information)
                     .EnableSensitiveDataLogging();
             }
