d523aef8354a6eeaef23f0a505953007cd144c97
dapie346 <dawid.pietruszewski@gmail.com>
2023-04-20 14:38:25 +0200

    Fixed descriptions

--- src/Data/CodecoolshopContext.cs
+++ src/Data/CodecoolshopContext.cs
@@ -87,7 +87,7 @@ namespace Data
                     DefaultPrice = 49.9m,
                     Currency = "USD",
                     Description =
-                        "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.",
+                        "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support",
                     ProductCategory = tablet,
                     Supplier = amazon
                 };
@@ -97,7 +97,7 @@ namespace Data
                     DefaultPrice = 479.0m,
                     Currency = "USD",
                     Description =
-                        "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.",
+                        "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand",
                     ProductCategory = tablet,
                     Supplier = lenovo
                 };
@@ -106,7 +106,7 @@ namespace Data
                     Name = "Amazon Fire HD 8",
                     DefaultPrice = 89.0m,
                     Currency = "USD",
-                    Description = "Amazon's latest Fire HD 8 tablet is a great value for media consumption.",
+                    Description = "Amazon's latest Fire HD 8 tablet is a great value for media consumption",
                     ProductCategory = tablet,
                     Supplier = amazon
                 };
@@ -115,7 +115,7 @@ namespace Data
                     Name = "MacBook Pro",
                     DefaultPrice = 1299.0m,
                     Currency = "USD",
-                    Description = "The MacBook Pro is a line of Macintosh portable computers introduced by Apple Inc. in January 2006.",
+                    Description = "The MacBook Pro is a line of Macintosh portable computers introduced by Apple Inc. in January 2006",
                     ProductCategory = laptop,
                     Supplier = apple
                 };
@@ -124,7 +124,7 @@ namespace Data
                     Name = "Samsung Galaxy S21",
                     DefaultPrice = 799.0m,
                     Currency = "USD",
-                    Description = "The Samsung Galaxy S21 is an Android-based smartphone designed, developed, and marketed by Samsung Electronics.",
+                    Description = "The Samsung Galaxy S21 is an Android-based smartphone designed, developed, and marketed by Samsung Electronics",
                     ProductCategory = smartphone,
                     Supplier = samsung
                 };
@@ -133,7 +133,7 @@ namespace Data
                     Name = "Apple iPhone 12",
                     DefaultPrice = 699.0m,
                     Currency = "USD",
-                    Description = "The iPhone 12 is a smartphone designed, developed, and marketed by Apple Inc.",
+                    Description = "The iPhone 12 is a smartphone designed, developed, and marketed by Apple Inc",
                     ProductCategory = smartphone,
                     Supplier = apple
                 };
@@ -142,7 +142,7 @@ namespace Data
                     Name = "Microsoft Surface Laptop",
                     DefaultPrice = 999.0m,
                     Currency = "USD",
-                    Description = "A slim, stylish laptop with a high-resolution touchscreen display and excellent battery life.",
+                    Description = "A slim, stylish laptop with a high-resolution touchscreen display and excellent battery life",
                     ProductCategory = laptop,
                     Supplier = microsoft
                 };
@@ -151,7 +151,7 @@ namespace Data
                     Name = "Apple MacBook Air",
                     DefaultPrice = 1199.0m,
                     Currency = "USD",
-                    Description = "A lightweight, portable laptop with a high-resolution Retina display and powerful performance.",
+                    Description = "A lightweight, portable laptop with a high-resolution Retina display and powerful performance",
                     ProductCategory = laptop,
                     Supplier = apple
                 };
@@ -160,7 +160,7 @@ namespace Data
                     Name = "Microsoft Surface Pro",
                     DefaultPrice = 799.0m,
                     Currency = "USD",
-                    Description = "A versatile 2-in-1 tablet and laptop with a high-resolution touchscreen display and excellent battery life.",
+                    Description = "A versatile 2-in-1 tablet and laptop with a high-resolution touchscreen display and excellent battery life",
                     ProductCategory = tablet,
                     Supplier = microsoft
                 };
@@ -169,7 +169,7 @@ namespace Data
                     Name = "Lenovo ThinkPad X1 Carbon",
                     DefaultPrice = 1299.0m,
                     Currency = "USD",
-                    Description = "A durable, high-performance business laptop with a sleek design and powerful performance.",
+                    Description = "A durable, high-performance business laptop with a sleek design and powerful performance",
                     ProductCategory = laptop,
                     Supplier = lenovo
                 };
@@ -178,7 +178,7 @@ namespace Data
                     Name = "Apple iPad Pro",
                     DefaultPrice = 799.0m,
                     Currency = "USD",
-                    Description = "A powerful tablet with a high-resolution display, advanced performance, and support for Apple Pencil.",
+                    Description = "A powerful tablet with a high-resolution display, advanced performance, and support for Apple Pencil",
                     ProductCategory = tablet,
                     Supplier = apple
                 };
@@ -187,7 +187,7 @@ namespace Data
                     Name = "Keurig K-Elite",
                     DefaultPrice = 169.0m,
                     Currency = "USD",
-                    Description = "A single-serve coffee maker with a strong brew setting and iced coffee capabilities.",
+                    Description = "A single-serve coffee maker with a strong brew setting and iced coffee capabilities",
                     ProductCategory = coffeeMaker,
                     Supplier = keurig
                 };
@@ -196,7 +196,7 @@ namespace Data
                     Name = "Breville Barista Express",
                     DefaultPrice = 599.0m,
                     Currency = "USD",
-                    Description = "A semi-automatic espresso machine with a built-in conical burr grinder and precise temperature controls.",
+                    Description = "A semi-automatic espresso machine with a built-in conical burr grinder and precise temperature controls",
                     ProductCategory = coffeeMaker,
                     Supplier = breville
                 };
@@ -205,7 +205,7 @@ namespace Data
                     Name = "Keurig K-Mini",
                     DefaultPrice = 79.0m,
                     Currency = "USD",
-                    Description = "A compact single-serve coffee maker, perfect for small spaces and on-the-go brewing.",
+                    Description = "A compact single-serve coffee maker, perfect for small spaces and on-the-go brewing",
                     ProductCategory = coffeeMaker,
                     Supplier = keurig
                 };
@@ -214,7 +214,7 @@ namespace Data
                     Name = "Breville Precision Brewer",
                     DefaultPrice = 279.0m,
                     Currency = "USD",
-                    Description = "A versatile drip coffee maker with customizable brew settings and a built-in cold brew mode.",
+                    Description = "A versatile drip coffee maker with customizable brew settings and a built-in cold brew mode",
                     ProductCategory = coffeeMaker,
                     Supplier = breville
                 };
