02737b58b3559d2a3c0c8593c1eebc89e786fde1
Jacek Walaszek <jwalaszek@centonit.com>
2023-05-02 10:08:06 +0200

    Add attribute to correctly display properties names

--- src/Domain/Order.cs
+++ src/Domain/Order.cs
@@ -15,15 +15,24 @@ namespace Domain
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid email")]
         public string Email { get; set; }
+
         [Required(ErrorMessage = "Phone is required")]
         [Phone(ErrorMessage = "invalid phone format")]
         public string Phone { get; set; }
+
+        [DisplayName("Billing Country")]
         [Required(ErrorMessage = "Billing country is required")]
         public string BillingCountry { get; set; }
+
+        [DisplayName("Billing City")]
         [Required(ErrorMessage = "Billing City is required")]
         public string BillingCity { get; set; }
+
+        [DisplayName("Billing Zip-Code")]
         [Required(ErrorMessage = "Billing Zip-Code is required")]
         public string BillingZipcode { get; set; }
+
+        [DisplayName("Billing Address")]
         [Required(ErrorMessage = "Billing Address is required")]
         public string BillingAddress { get; set; }
 
@@ -31,15 +40,19 @@ namespace Domain
         [NotMapped]
         public bool ShippingSameAsBilling { get; set; }
 
+        [DisplayName("Shipping Country")]
         [RequiredIf("ShippingSameAsBilling", false, ErrorMessage = "Shipping Country is required")]
         public string ShippingCountry { get; set; }
 
+        [DisplayName("Shipping City")]
         [RequiredIf("ShippingSameAsBilling", false, ErrorMessage = "Shipping City is required")]
         public string ShippingCity { get; set; }
 
+        [DisplayName("Shipping Zip-Code")]
         [RequiredIf("ShippingSameAsBilling", false, ErrorMessage = "Shipping ZIP Code is required")]
         public string ShippingZipcode { get; set; }
 
+        [DisplayName("Shipping Address")]
         [RequiredIf("ShippingSameAsBilling", false, ErrorMessage = "Shipping Address is required")]
         public string ShippingAddress { get; set; }
     }
