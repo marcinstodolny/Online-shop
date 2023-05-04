ae568a5c26ee666c114cb7e2434f3cfd4f523e8d
Jacek Walaszek <jwalaszek@centonit.com>
2023-05-04 16:52:07 +0200

    Add pay button after selecting the payment option

--- src/Codecool.CodecoolShop/wwwroot/js/site.js
+++ src/Codecool.CodecoolShop/wwwroot/js/site.js
@@ -42,13 +42,16 @@ function validate() {
 
 // ********* | Payment site | ************
 function showCreditCardDetails() {
+    let payButton = document.getElementById("pay-button");
     let creditCardDetails = document.getElementById("credit-card-details");
     let checkbox = document.getElementById("credit-card-checkbox");
     if (checkbox.checked) {
         creditCardDetails.style.display = "block";
-        checkbox.value = "true";
+        payButton.style.display = "block";
+        checkbox.innerHTML = "true";
     } else {
         creditCardDetails.style.display = "none";
-        checkbox.value = "false";
+        payButton.style.display = "none";
+        checkbox.innerHTML = "false";
     }
 }
