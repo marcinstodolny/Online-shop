81e572ace5cb382e03cf9dc2cca926774ba2f7b4
Jacek Walaszek <jwalaszek@centonit.com>
2023-05-04 16:58:32 +0200

    Add seperate JavaScript file for Payment html site

---
+++ src/Codecool.CodecoolShop/wwwroot/js/Payment.js
@@ -0,0 +1,14 @@
+﻿function showCreditCardDetails() {
+    let payButton = document.getElementById("pay-button");
+    let creditCardDetails = document.getElementById("credit-card-details");
+    let checkbox = document.getElementById("credit-card-checkbox");
+    if (checkbox.checked) {
+        creditCardDetails.style.display = "block";
+        payButton.style.display = "block";
+        checkbox.innerHTML = "true";
+    } else {
+        creditCardDetails.style.display = "none";
+        payButton.style.display = "none";
+        checkbox.innerHTML = "false";
+    }
+}
