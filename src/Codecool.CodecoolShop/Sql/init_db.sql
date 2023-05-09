fa6cc2446bf42c56a68cb97ab7a43f62f37fb2e1
Jacek Walaszek <jwalaszek@centonit.com>
2023-05-09 13:05:52 +0200

    Create script for initial sql DB creation

---
+++ src/Codecool.CodecoolShop/Sql/init_db.sql
@@ -0,0 +1,147 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230418083938_InitialMigration')
+BEGIN
+    CREATE TABLE [ProductCategories] (
+        [Id] int NOT NULL IDENTITY,
+        [Department] nvarchar(max) NOT NULL,
+        [Name] nvarchar(max) NOT NULL,
+        [Description] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_ProductCategories] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230418083938_InitialMigration')
+BEGIN
+    CREATE TABLE [Suppliers] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [Description] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Suppliers] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230418083938_InitialMigration')
+BEGIN
+    CREATE TABLE [Products] (
+        [Id] int NOT NULL IDENTITY,
+        [Currency] nvarchar(max) NOT NULL,
+        [DefaultPrice] decimal(18,2) NOT NULL,
+        [ProductCategoryId] int NOT NULL,
+        [SupplierId] int NOT NULL,
+        [Name] nvarchar(max) NOT NULL,
+        [Description] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Products] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Products_ProductCategories_ProductCategoryId] FOREIGN KEY ([ProductCategoryId]) REFERENCES [ProductCategories] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_Products_Suppliers_SupplierId] FOREIGN KEY ([SupplierId]) REFERENCES [Suppliers] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230418083938_InitialMigration')
+BEGIN
+    CREATE INDEX [IX_Products_ProductCategoryId] ON [Products] ([ProductCategoryId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230418083938_InitialMigration')
+BEGIN
+    CREATE INDEX [IX_Products_SupplierId] ON [Products] ([SupplierId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230418083938_InitialMigration')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20230418083938_InitialMigration', N'7.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230420100643_withoutProductList')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20230420100643_withoutProductList', N'7.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230428120123_AddOrder')
+BEGIN
+    CREATE TABLE [Orders] (
+        [Id] int NOT NULL IDENTITY,
+        [Email] nvarchar(max) NOT NULL,
+        [Phone] nvarchar(max) NOT NULL,
+        [BillingCountry] nvarchar(max) NOT NULL,
+        [BillingCity] nvarchar(max) NOT NULL,
+        [BillingZipcode] nvarchar(max) NOT NULL,
+        [BillingAddress] nvarchar(max) NOT NULL,
+        [ShippingCountry] nvarchar(max) NOT NULL,
+        [ShippingCity] nvarchar(max) NOT NULL,
+        [ShippingZipcode] nvarchar(max) NOT NULL,
+        [ShippingAddress] nvarchar(max) NOT NULL,
+        [Name] nvarchar(max) NOT NULL,
+        [Description] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Orders] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230428120123_AddOrder')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20230428120123_AddOrder', N'7.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230504064014_RemoveDescriptionFromOrder')
+BEGIN
+    DECLARE @var0 sysname;
+    SELECT @var0 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Orders]') AND [c].[name] = N'Description');
+    IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [Orders] DROP CONSTRAINT [' + @var0 + '];');
+    ALTER TABLE [Orders] DROP COLUMN [Description];
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230504064014_RemoveDescriptionFromOrder')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20230504064014_RemoveDescriptionFromOrder', N'7.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
