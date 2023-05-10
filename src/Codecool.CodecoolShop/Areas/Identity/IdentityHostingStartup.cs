a644522580ed777c6312d3e6f8d7db3589fe9f8e
TomaszCitko <citkotomasz@gmail.com>
2023-05-10 13:53:21 +0200

    Packages

---
+++ src/Codecool.CodecoolShop/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Codecool.CodecoolShop.Areas.Identity.Data;
+using Codecool.CodecoolShop.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Codecool.CodecoolShop.Areas.Identity.IdentityHostingStartup))]
+namespace Codecool.CodecoolShop.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CodecoolCodecoolShopContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CodecoolCodecoolShopContextConnection")));
+
+                services.AddDefaultIdentity<Customer>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<CodecoolCodecoolShopContext>();
+            });
+        }
+    }
+}
