1e1c19f4a5c53c4a32c35497a7c75b3552ea4877
TomaszCitko <citkotomasz@gmail.com>
2023-05-10 14:02:44 +0200

    Razor pages created

---
+++ src/Codecool.CodecoolShop/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Codecool.CodecoolShop.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Codecool.CodecoolShop.Areas.Identity.IdentityHostingStartup))]
+namespace Codecool.CodecoolShop.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CodecoolCodecoolShopContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CodecoolCodecoolShopContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<CodecoolCodecoolShopContext>();
+            });
+        }
+    }
+}
