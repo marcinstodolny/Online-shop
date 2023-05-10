bdafbf25c35b098e1b4d87efa198ad8fccd918c3
Jacek Walaszek <jwalaszek@centonit.com>
2023-05-10 12:26:50 +0200

    Add exceptions to order controller

--- src/Codecool.CodecoolShop/Controllers/OrderController.cs
+++ src/Codecool.CodecoolShop/Controllers/OrderController.cs
@@ -29,38 +29,57 @@ namespace Codecool.CodecoolShop.Controllers
         [HttpPost]
         public IActionResult Checkout(Order order)
         {
-            order.CreatedAt = DateTime.Now;
-            order.Status = "Checked";
-            _orderService.SaveOrderToJson(order);
-
-            if (order.ShippingSameAsBilling)
-            {
-                order.ShippingAddress = order.BillingAddress;
-                order.ShippingCity = order.BillingCity;
-                order.ShippingCountry = order.BillingCountry;
-                order.ShippingZipcode = order.BillingZipcode;
-            }
-            if (ModelState.IsValid)
+            try
             {
                 order.CreatedAt = DateTime.Now;
-                order.Status = "Confirmed";
+                order.Status = "Checked";
                 _orderService.SaveOrderToJson(order);
 
-                HttpContext.Session.SetObjectAsJson("orderDetails", order);
-                _logger.LogInformation($"Checkout order completed for order id: {order.Id}");
-                return RedirectToAction("Payment");
+                if (order.ShippingSameAsBilling)
+                {
+                    order.ShippingAddress = order.BillingAddress;
+                    order.ShippingCity = order.BillingCity;
+                    order.ShippingCountry = order.BillingCountry;
+                    order.ShippingZipcode = order.BillingZipcode;
+                }
+                if (ModelState.IsValid)
+                {
+                    order.CreatedAt = DateTime.Now;
+                    order.Status = "Confirmed";
+                    _orderService.SaveOrderToJson(order);
+
+                    HttpContext.Session.SetObjectAsJson("orderDetails", order);
+                    _logger.LogInformation($"Checkout order completed for order id: {order.Id}");
+                    return RedirectToAction("Payment");
+                }
+                _logger.LogInformation($"Checkout order not valid for order id: {order.Id}");
+
+                return View("Checkout");
             }
-            _logger.LogInformation($"Checkout order not valid for order id: {order.Id}");
+            catch (Exception e)
+            {
+                _logger.LogError($"An error occurred during the checkout process: {e.Message}");
+            }
+            
             return View("Checkout");
         }
 
         public IActionResult Payment()
         {
-            var cart = HttpContext.Session.GetObjectFromJson<List<Item>>("cart");
-            var totalPrice = cart.Sum(item => item.Product.DefaultPrice * item.Quantity);
-            ViewBag.TotalPrice = totalPrice;
-            ViewBag.paymentMessage = HttpContext.Session.GetObjectFromJson<string>("paymentMessage");
-            HttpContext.Session.Remove("paymentMessage");
+            try
+            {
+                var cart = HttpContext.Session.GetObjectFromJson<List<Item>>("cart");
+                var totalPrice = cart.Sum(item => item.Product.DefaultPrice * item.Quantity);
+                ViewBag.TotalPrice = totalPrice;
+                ViewBag.paymentMessage = HttpContext.Session.GetObjectFromJson<string>("paymentMessage");
+                HttpContext.Session.Remove("paymentMessage");
+
+                return View();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"An error occurred while loading the payment page: {e.Message}");
+            }
 
             return View();
         }
@@ -68,47 +87,66 @@ namespace Codecool.CodecoolShop.Controllers
         [HttpPost]
         public IActionResult Payment(CreditCard creditCard)
         {
-            if (ModelState.IsValid)
+            try
             {
+                if (ModelState.IsValid)
+                {
 
-                _logger.LogError($"Payment details are correct for the credit card number {creditCard.CardNumber}");
-                return RedirectToAction("Confirmation");
+                    _logger.LogError($"Payment details are correct for the credit card number {creditCard.CardNumber}");
+                    return RedirectToAction("Confirmation");
+                }
+                var order = HttpContext.Session.GetObjectFromJson<Order>("orderDetails");
+
+                order.CreatedAt = DateTime.Now;
+                order.Status = "Failed payment";
+                _orderService.SaveOrderToJson(order);
+
+                var lastFourDigits = 4;
+                var paymentMessage =
+                    $"Payment details are incorrect or invalid for the credit card ending number {creditCard.CardNumber.Substring(creditCard.CardNumber.Length - lastFourDigits)}";
+                _logger.LogError($"Payment details are incorrect or invalid for the credit card number {creditCard.CardNumber}");
+                HttpContext.Session.SetObjectAsJson("paymentMessage", paymentMessage);
+
+                return RedirectToAction("Payment");
             }
-            var order = HttpContext.Session.GetObjectFromJson<Order>("orderDetails");
+            catch (Exception e)
+            {
+                _logger.LogError($"An error occurred during the payment process: {e.Message}");
+            }
 
-            order.CreatedAt = DateTime.Now;
-            order.Status = "Failed payment";
-            _orderService.SaveOrderToJson(order);
-
-            var lastFourDigits = 4;
-            var paymentMessage =
-                $"Payment details are incorrect or invalid for the credit card ending number {creditCard.CardNumber.Substring(creditCard.CardNumber.Length - lastFourDigits)}";
-            _logger.LogError($"Payment details are incorrect or invalid for the credit card number {creditCard.CardNumber}");
-            HttpContext.Session.SetObjectAsJson("paymentMessage", paymentMessage);
             return RedirectToAction("Payment");
         }
 
         public IActionResult Confirmation()
         {
-            var order = HttpContext.Session.GetObjectFromJson<Order>("orderDetails");
-            order.CreatedAt = DateTime.Now;
-            order.Status = "Payed";
-            _orderService.SaveOrderToJson(order);
+            try
+            {
+                var order = HttpContext.Session.GetObjectFromJson<Order>("orderDetails");
+                order.CreatedAt = DateTime.Now;
+                order.Status = "Payed";
+                _orderService.SaveOrderToJson(order);
 
-            var cart = HttpContext.Session.GetObjectFromJson<List<Item>>("cart") ?? new List<Item>();
-            ViewBag.cart = cart;
-            ViewBag.total = cart.Sum(item => item.Product.DefaultPrice * item.Quantity);
-            //var order = _orderService.GetAllOrders();
+                var cart = HttpContext.Session.GetObjectFromJson<List<Item>>("cart") ?? new List<Item>();
+                ViewBag.cart = cart;
+                ViewBag.total = cart.Sum(item => item.Product.DefaultPrice * item.Quantity);
+                //var order = _orderService.GetAllOrders();
 
-            HttpContext.Session.Remove("cart");
-            HttpContext.Session.Remove("orderDetails");
+                HttpContext.Session.Remove("cart");
+                HttpContext.Session.Remove("orderDetails");
 
-            // Send the email confirmation
-            //_orderService.SendEmailConfirmation(order, ViewBag.total); //; this is an example to configure SMTP client instance to send confirmation email
+                // Send the email confirmation
+                //_orderService.SendEmailConfirmation(order, ViewBag.total); //; this is an example to configure SMTP client instance to send confirmation email
 
-            _orderService.AddOrder(order);
+                _orderService.AddOrder(order);
 
-            return View(order);
+                return View(order);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"An error occurred during the confirmation process: {e.Message}");
+            }
+
+            return RedirectToAction("Payment");
         }
     }
 
