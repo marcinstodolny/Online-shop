b84919ed8c9951c555e5fcd88b0dfc88651c7756
Marcin Stodolny <marcin.stodolny@o2.pl>
2023-09-09 02:22:42 +0200

    updated privancy page, changes names to Online Shop, removed small mistakes

--- src/Codecool.CodecoolShop/Services/EmailService.cs
+++ src/Codecool.CodecoolShop/Services/EmailService.cs
@@ -29,7 +29,7 @@ namespace Codecool.CodecoolShop.Services
             message.To.Add(new MailAddress(order.Email));
 
             message.Subject = "Order Confirmation";
-            message.Body = $"Dear {order.Name},\n\nThank you for your order. Your order has been confirmed.\n\nShipping Address:\n{order.ShippingAddress}\n\nTotal amount: {total}\n\nBest Regards,\nYour CodeCoolShop";
+            message.Body = $"Dear {order.Name},\n\nThank you for your order. Your order has been confirmed.\n\nShipping Address:\n{order.ShippingAddress}\n\nTotal amount: {total}\n\nBest Regards,\nYour Online Shop";
 
             client.Send(message);
         }
